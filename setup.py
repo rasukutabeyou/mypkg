1aeb050aafdc27d4b16f85822b1336cdea4a3373
Kouta Sakai <K14sakai@gmail.com>
2024-12-29 17:19:21 +0900

    ??????

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/msg', ['msg/Person.msg']),
         (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
