159be912bb0556d4f470aa5aae41cf2372c8b334
Kouta Sakai <K14sakai@gmail.com>
2025-01-02 16:05:50 +0900

    add launch

---
+++ launch/coordinate_listen.launch.py
@@ -0,0 +1,19 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+def generate_launch_description():
+
+    coordinater = launch_ros.actions.Node(
+        package='mypkg',      #パッケージの名前を指定
+        executable='coordinater',  #実行するファイルの指定
+        )
+    listener = launch_ros.actions.Node(
+        package='mypkg',
+        executable='listener',
+        output='screen'        #ログを端末に出すための設定
+        )
+
+    return launch.LaunchDescription([coordinater, listener])  
