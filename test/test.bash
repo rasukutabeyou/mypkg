b2db874e16190f05612d1255efbf62968b431d23
Kouta Sakai <K14sakai@gmail.com>
2025-01-02 15:55:43 +0900

    test ver2.0

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,27 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 30 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 16 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+wng () {
+         echo warnig${1}
+	 res=1
+}
+
+res=0
 
 cat /tmp/mypkg.log |
+grep '(x,y,z): 1.000000,1.000000,1.000000'
+[ "$?" = 0 ] || wng "1.000000,1.000000,1.000000"
+cat /tmp/mypkg.log |
+grep '(x,y,z): 10.000000,2.000000,1.000000'
+[ "$?" = 0 ] || wng "(x,y,z): 10.000000,2.000000,1.000000"
+cat /tmp/mypkg.log |
+grep '(x,y,z): 10.000000,10.000000,2.000000'
+[ "$?" = 0 ] || wng "(x,y,z): 10.000000,10.000000,2.000000"
+cat /tmp/mypkg.log |
 grep '(x,y,z): 10.000000,10.000000,10.000000'
+[ "$?" = 0 ] || wng "(x,y,z): 10.000000,10.000000,10.000000"
+
+
+exit $res
