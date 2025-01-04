7593e2168175b2441ecfc6b4affaec834931c54f
Kouta Sakai <K14sakai@gmail.com>
2025-01-05 02:24:23 +0900

    write SPDX

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025 Kouta Sakai
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Point
--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2025 Kouta Sakai
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
