b66f2de1de8453610f61e0b7e16f07d7d3a57ee0
Kouta Sakai <K14sakai@gmail.com>
2025-01-05 02:08:33 +0900

    test ver2.0

--- test/test.bash
+++ test/test.bash
@@ -7,7 +7,7 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 echo $ROS_DISTRO
-timeout 16 ros2 launch mypkg coordinate_listen.launch.py > /tmp/mypkg.log
+timeout 12 ros2 launch mypkg coordinate_listen.launch.py > /tmp/mypkg.log
 
 wng () {
          echo warnig${1}
@@ -17,17 +17,32 @@ wng () {
 res=0
 
 cat /tmp/mypkg.log |
-grep '(x,y,z): 1.000000,1.000000,1.000000'
-[ "$?" = 0 ] || wng "1.000000,1.000000,1.000000"
+grep '43.062100,141.354400'
+[ "$?" = 0 ] || wng "札幌"
 cat /tmp/mypkg.log |
-grep '(x,y,z): 10.000000,2.000000,1.000000'
-[ "$?" = 0 ] || wng "(x,y,z): 10.000000,2.000000,1.000000"
+grep '38.268200,140.869400'
+[ "$?" = 0 ] || wng "仙台"
 cat /tmp/mypkg.log |
-grep '(x,y,z): 10.000000,10.000000,2.000000'
-[ "$?" = 0 ] || wng "(x,y,z): 10.000000,10.000000,2.000000"
+grep '35.689500,139.691700'
+[ "$?" = 0 ] || wng "東京"
 cat /tmp/mypkg.log |
-grep '(x,y,z): 10.000000,10.000000,10.000000'
-[ "$?" = 0 ] || wng "(x,y,z): 10.000000,10.000000,10.000000"
+grep '35.443700,139.638000'
+[ "$?" = 0 ] || wng "横浜"
+cat /tmp/mypkg.log |
+grep '35.181500,136.906600'
+[ "$?" = 0 ] || wng "名古屋"
+cat /tmp/mypkg.log |
+grep '34.693700,135.502300'
+[ "$?" = 0 ] || wng "大阪"
+cat /tmp/mypkg.log |
+grep '35.011600,135.768100'
+[ "$?" = 0 ] || wng "京都"
+cat /tmp/mypkg.log |
+grep '34.690100,135.195600'
+[ "$?" = 0 ] || wng "神戸"
+cat /tmp/mypkg.log |
+grep '33.590400,130.401700'
+[ "$?" = 0 ] || wng "福岡"
 
 
 exit $res
